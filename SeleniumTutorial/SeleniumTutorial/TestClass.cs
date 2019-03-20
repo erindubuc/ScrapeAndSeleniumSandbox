836d3cc84e6f49efd4c28a71c82c6ac0fde0a69d
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-03-20 15:28:16 -0400

    Executed selenium c# automated scripts

---
+++ SeleniumTutorial/SeleniumTutorial/TestClass.cs
@@ -0,0 +1,41 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumTutorial.BaseClass;
+
+namespace SeleniumTutorial
+{
+    [TestFixture]
+    public class TestClass : BaseTest
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            //IWebDriver driver = new ChromeDriver();
+            //driver.Url = "https://www.facebook.com/";
+            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Selenium C#");
+            //driver.Quit();
+        }
+
+        [Test]
+        public void TestMethod2()
+        { 
+            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Selenium C#");
+        }
+
+        [Test]
+        public void TestMethod3()
+        {
+            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Selenium C#");
+            Thread.Sleep(5000);
+        }
+    }
+}
