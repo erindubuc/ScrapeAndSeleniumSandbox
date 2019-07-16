48274eb4844cb4eb58ab2032f9e09d8792069324
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-07-16 16:19:11 -0400

    Getting innerhtml, innertext, outerhtml, & name of parent node

--- HAP_Examples/HAP_Examples/Program.cs
+++ HAP_Examples/HAP_Examples/Program.cs
@@ -11,7 +11,20 @@ namespace HAP_Examples
     {
         static void Main(string[] args)
         {
+            var html =
+                @"<body>
+                <h1>.Net Core</h1>
+                <p>
+                This is <b>C#, ASP.Net</b> paragraph
+                </p>
+                <h1>.Net Core with Angular</h1>
+                <p>
+                This is <b>HTML Agility Pack</b> sample
+                </p>
+                </body>";
 
+
+            /*
             var html = @"
                 <td>
                 </td>
@@ -23,9 +36,44 @@ namespace HAP_Examples
                 <span> Can I get this?
                 </span>
                 </td>";
+            */
 
             var htmlDoc = new HtmlDocument();
             htmlDoc.LoadHtml(html);
+
+            var htmlNodes = htmlDoc.DocumentNode.SelectNodes("//body/p");
+
+
+            // Gets InnerHTML content present within the boundaries of opening 
+            // and closing tags of the mentioned HTML object
+            Console.WriteLine("InnerHtml: ");
+            foreach (var node in htmlNodes)
+                Console.WriteLine(node.InnerHtml);
+
+            // InnerText gets text between the opening and closing tags of 
+            // the desired HTML object
+            Console.WriteLine("InnerText: ");
+            foreach (var node in htmlNodes)
+                Console.WriteLine(node.InnerText);
+
+            // OuterHtml get the object as well as the contents inside
+            Console.WriteLine("OuterHtml: ");
+            foreach (var node in htmlNodes)
+                Console.WriteLine(node.OuterHtml);
+
+
+            var headerNodes = htmlDoc.DocumentNode.SelectNodes("//body/h1");
+            Console.WriteLine("Header Nodes OuterHtml" +
+                ": ");
+            foreach (var node in headerNodes)
+                Console.WriteLine(node.OuterHtml);
+
+
+            // Get parent node
+            var parent = htmlDoc.DocumentNode.SelectSingleNode("//body/h1");
+
+            HtmlNode parentNode = parent.ParentNode;
+            Console.WriteLine($"The name of the parent node is: {parentNode.Name}");
 
             /*
             // SELECT ALL NODES that match with the expressions of the type 
@@ -38,13 +86,15 @@ namespace HAP_Examples
             */
 
 
+            /*
             // SELECT FIRST NODE that matches the given XPath expression
             //var node = htmlDoc.DocumentNode.SelectNodes("//td/input").First()
             //    .Attributes["value"].Value;
             var node = htmlDoc.DocumentNode.SelectSingleNode("//td/span");
             Console.WriteLine(node.InnerText);
-            
-            
+            */
+
+
             /*
             // Loading html as document and getting innertext
             string url = "https://technologycrowds.com";
@@ -52,6 +102,8 @@ namespace HAP_Examples
             HtmlDocument doc = new HtmlDocument();
             doc = web.Load(url);
             */
+
+
             Console.ReadLine();
         }
     }
